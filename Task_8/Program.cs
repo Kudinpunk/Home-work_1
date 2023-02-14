1ab2b2e29b6cffe65ea94f424053bb90e9343774
Kudinpunk <kudinpunk@yandex.ru>
2023-02-14 18:30:03 +0300

    8

---
+++ Task_8/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая на вход принимает число (N), а
+ на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8  */
+
+Console.Write($"Input your number: ") ;
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current_number = 2;
+
+while (current_number <= number)
+{
+    Console.Write(current_number + " ");
+    current_number += 2;
+}
