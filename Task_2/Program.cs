cff516ef7bac587dcd89f8d5183718313c45d484
Kudinpunk <kudinpunk@yandex.ru>
2023-02-14 17:47:47 +0300

    2

---
+++ Task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 2: Напишите программу, которая на вход принимает два 
+числа и выдаёт, какое число большее, а какое меньшее.
+a = 5; b = 7 -> max = 7, min = 5
+a = 2 b = 10 -> max = 10, min = 2 */
+
+Console.Write($"Input first number: ") ;
+int first_number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"Input second number: ");
+int second_number = Convert.ToInt32(Console.ReadLine());
+
+if (first_number > second_number)
+{
+    Console.WriteLine($"max = {first_number} min = {second_number}");
+}
+else
+{
+    Console.WriteLine($"max = {second_number} min = {first_number}");
+}
