4c3cb9fe706110bc8d788d45825a587b5b849400
Kudinpunk <kudinpunk@yandex.ru>
2023-02-17 12:04:54 +0300

    13

---
+++ Task_13/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу, которая выводит третью цифру 
+заданного числа или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/ 
+
+Console.WriteLine("Input your number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+string third_symbol = Convert.ToString(user_number);
+
+if (third_symbol.Length > 2)
+    Console.WriteLine($"Second symbol is {third_symbol[2]}");
+else
+    Console.WriteLine("Your number is not right.");
