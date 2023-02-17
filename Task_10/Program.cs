46a605510ec7ff76175316360bdc6d88fa249c94
Kudinpunk <kudinpunk@yandex.ru>
2023-02-17 10:36:36 +0300

    10

---
+++ Task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Напишите программу, которая принимает на вход 
+трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 
+*/
+
+Console.WriteLine("Input your number: ");
+int user_num = Convert.ToInt32(Console.ReadLine());
+
+if (user_num > 100 & user_num < 1000)
+{
+    int second_num = user_num % 100 / 10;
+    Console.WriteLine($"Second digit in {user_num} is {second_num}.");
+}
+else
+    Console.Write("Your number is not right.");
