dd7569174ad9c3c19c3b8ccd9c6377434a7d3606
Kudinpunk <kudinpunk@yandex.ru>
2023-02-17 12:25:51 +0300

    15

---
+++ Task_15/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Напишите программу, которая принимает на вход цифру, обозначающую 
+день недели, и проверяет, является ли этот день выходным.
+6 -> да
+7 -> да
+1 -> нет */ 
+
+Console.WriteLine("Inpunt the day of the week...");
+int user_day = Convert.ToInt32(Console.ReadLine());
+
+if (user_day > 0 & user_day < 8)
+{
+    if (user_day == 6 || user_day == 7)
+        Console.WriteLine("YES");
+    else
+        Console.WriteLine("NO");
+}
+else
+    Console.WriteLine("Input the day of the week between 1 and 7.");
